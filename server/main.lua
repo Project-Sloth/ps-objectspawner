24dea8727a347d010abd6d46d578c9bfd37d05b2
Devin Monro <mage1d@yahoo.com>
2022-05-03 00:00:52 -0400

    Fixed core export

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports["urp-core"]:GetCoreObject()
+local QBCore = exports["qb-core"]:GetCoreObject()
 local ServerObjects = {}
 
 RegisterNetEvent("objects:CreateNewObject", function(model, coords)
